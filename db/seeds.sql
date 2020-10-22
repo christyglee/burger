7b8547623cd4d1fb0379f596304fa5b19528cecd
unknown <christy.g.lee@gmail.com>
2020-10-21 19:12:45 -0700

    updated schema and added seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Baconator', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Animal Style Double-Double', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('McDouble', false);
