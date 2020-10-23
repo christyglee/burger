de327ad51c2ef8b8309ec83d9f597f48fbfa814d
unknown <christy.g.lee@gmail.com>
2020-10-23 01:36:38 -0700

    init commit

--- config/connection.js
+++ config/connection.js
@@ -5,9 +5,12 @@ const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "password",
+  password: "",
   database: "burgers_db"
 });
+
+// for Heroku
+// var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
 // Make connection.
 connection.connect(function(err) {
